39dcf4011d61af796b41aff0079326348a756a28
user <user@iMac-user-1.local>
2022-02-22 16:55:55 +0400

    add some

--- app/src/main/java/com/example/wsruserfood/ui/foods/FoodsFragment.kt
+++ app/src/main/java/com/example/wsruserfood/ui/foods/FoodsFragment.kt
@@ -6,6 +6,10 @@ import com.example.wsruserfood.ui.core.BaseFragment
 import android.view.ViewGroup
 import androidx.activity.addCallback
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.wsruserfood.R
+import com.example.wsruserfood.extensions.gone
+import com.example.wsruserfood.extensions.visible
 
 import com.example.wsruserfood.viewmodel.main.MainViewModel
 
@@ -20,9 +24,34 @@ class FoodsFragment: BaseFragment<MainViewModel, FragmentFoodsBinding>() {
         FragmentFoodsBinding.inflate(inflater, container, false)
 
     override fun setupViews() {
+        binding.eatBig.gone()
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
             requireActivity().finish()
             mainActivity.showBottomMenu()
+        }
+        with(binding) {
+            eat1.setOnClickListener{
+                eat1.gone()
+                eat2.gone()
+                eat3.gone()
+                eat4.gone()
+                food1.gone()
+                food2.gone()
+                food3.gone()
+                food4.gone()
+                eatBig.visible()
+            }
+            btnBack.setOnClickListener{
+                eat1.visible()
+                eat2.visible()
+                eat3.visible()
+                eat4.visible()
+                food1.visible()
+                food2.visible()
+                food3.visible()
+                food4.visible()
+                eatBig.gone()
+            }
         }
     }
 }
