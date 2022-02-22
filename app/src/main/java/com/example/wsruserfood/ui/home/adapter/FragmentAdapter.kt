33e8be6d5820e8c64ac96c25b75435437e25d489
user <user@iMac-user-1.local>
2022-02-22 15:28:50 +0400

    add main

---
+++ app/src/main/java/com/example/wsruserfood/ui/home/adapter/FragmentAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.wsruserfood.ui.home.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class FragmentAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager, lifecycle) {
+    private val fragmentList : MutableList<Fragment> =ArrayList()
+    private val titleList : MutableList<String> =ArrayList()
+
+    override fun getItemCount(): Int {
+        return fragmentList.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return  fragmentList[position]
+    }
+
+
+    fun addFragment(fragment: Fragment, title: String){
+        fragmentList.add(fragment)
+        titleList.add(title)
+    }
+
+    fun getPageTitle(position: Int): CharSequence? {
+        return titleList[position]
+    }
+}
--- app/src/main/java/com/example/wsruserfood/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/wsruserfood/ui/main/MainFragment.kt
@@ -6,6 +6,12 @@ import com.example.wsruserfood.ui.core.BaseFragment
 import android.view.ViewGroup
 import androidx.activity.addCallback
 import androidx.fragment.app.viewModels
+import com.example.wsruserfood.R
+import com.example.wsruserfood.ui.drinks.DrinksFragment
+import com.example.wsruserfood.ui.foods.FoodsFragment
+import com.example.wsruserfood.ui.home.adapter.FragmentAdapter
+import com.example.wsruserfood.ui.sauce.SauceFragment
+import com.example.wsruserfood.ui.snacks.SnacksFragment
 import com.example.wsruserfood.viewmodel.main.MainViewModel
 import com.google.android.material.tabs.TabLayoutMediator
 
@@ -28,23 +34,24 @@ class MainFragment: BaseFragment<MainViewModel, FragmentMainBinding>() {
 
     override fun onStart() {
         super.onStart()
-//        setupTabLayoutAndPager()
+        setupTabLayoutAndPager()
     }
 
-//    private fun setupTabLayoutAndPager() {
-//        with(binding) {
-//            val adapter = FragmentAdapter(childFragmentManager, lifecycle)
-//
-//            adapter.addFragment(TimetableFragment(), getString(R.string.tab_timetable))
-//            adapter.addFragment(HomeworkFragment(), getString(R.string.tab_homework))
-//            adapter.addFragment(WeatherFragment(), getString(R.string.tab_weather))
-//
-//            pager.adapter = adapter
-//            TabLayoutMediator(tabLayout, pager) { tab, position ->
-//                tab.text = adapter.getPageTitle(position)
-//            }.attach()
-//
-//        }
-//    }
+    private fun setupTabLayoutAndPager() {
+        with(binding) {
+            val adapter = FragmentAdapter(childFragmentManager, lifecycle)
+
+            adapter.addFragment(FoodsFragment(), getString(R.string.foods))
+            adapter.addFragment(DrinksFragment(), getString(R.string.drinks))
+            adapter.addFragment(SnacksFragment(), getString(R.string.snacks))
+            adapter.addFragment(SauceFragment(), getString(R.string.sauce))
+
+            pager.adapter = adapter
+            TabLayoutMediator(tabLayout, pager) { tab, position ->
+                tab.text = adapter.getPageTitle(position)
+            }.attach()
+
+        }
+    }
 
 }
